141cff4f2a276c38e6c538869797cf06f2661581
Dzmitrysiniakou <jack.pott@mail.ru>
2020-03-10 15:09:29 +0300

    feat brackets done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsList = [];
+  if (str.length == 0 || str.length % 2 != 0){
+    return false;
+  } else {
+    for(let i=0; i < bracketsConfig.length;){
+      bracketsList = bracketsConfig[i].join('');
+      console.log(bracketsList);
+      if(str.includes(bracketsList)){
+        str = str.replace(bracketsList, '');
+        console.log(str);
+        i = 0;
+      } else {i++};
+    }
+    if(!str.length) {
+      return true;
+    } else {
+      return false;
+    }
+  } 
 }
+
